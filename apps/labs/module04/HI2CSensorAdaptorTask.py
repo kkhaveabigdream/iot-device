ff61a5661825b28a2cedef55e83cf704fd1b88cb
kkhaveabigdream <shen.k@husky.neu.edu>
2020-02-14 17:17:51 -0500

    lab04 first commit

--- apps/labs/module04/HI2CSensorAdaptorTask.py
+++ apps/labs/module04/HI2CSensorAdaptorTask.py
@@ -7,11 +7,14 @@ Created on Feb 13, 2020
 import threading
 
 
-#import smbus2
+import smbus2
 from labs.common.ConfigUtil import ConfigUtil
 from labs.common import ConfigConst
+import logging
+from time import sleep
 
-#i2cBus          = smbus2.SMBus(1)
+
+i2cBus          = smbus2.SMBus(1)
 
 enableControl   = 0x2D
 enableMeasure   = 0x08
@@ -25,6 +28,7 @@ begAddr         = 0x28
 totBytes        = 6
 
 DEFAULT_RATE_IN_SEC = 5
+rateInSec           = 5
 
 class HI2CSensorAdaptorTask(threading.Thread):
     '''
@@ -32,12 +36,47 @@ class HI2CSensorAdaptorTask(threading.Thread):
     '''
 
 
-    def __init__(self):
+    def __init__(self,rateInSec = 5):
         super(HI2CSensorAdaptorTask,self).__init__()
         
+        self.rateInSec = rateInSec
         self.config = ConfigUtil()
         self.config.loadConfig(ConfigConst.DEFAULT_CONFIG_FILE_NAME)
         
         print('Configuration data...\n' + str(ConfigConst.DEFAULT_CONFIG_FILE_NAME))
+        
+    def initI2CBus(self):
+        logging.info("Initializing I2C bus and enabling I2C addresses...")
+        
+        i2cBus.write_byte_data(accelAddr, 0, 0)
+        i2cBus.write_byte_data(magAddr,0,0)
+        i2cBus.write_byte_data(pressAddr,0,0)
+        i2cBus.write_byte_data(humidAddr,0,0)
+        
+    def displayAccelerometerData(self):
+        acceleData = i2cBus.read_byte_data(accelAddr, begAddr, totBytes)
+        print(acceleData)
+        
+    def displayMagnetometerData(self):
+        magData = i2cBus.read_byte_data(magAddr, begAddr, totBytes)
+        print(magData)
+    
+    def displayPressureData(self):
+        pressureData = i2cBus.read_byte_data(pressAddr, begAddr, totBytes)
+        print(pressureData)
+        
+    def displayHumidityData(self):
+        humidityData = i2cBus.read_byte_data(humidAddr, begAddr, totBytes)
+        print(humidityData)
+        
+    def run(self):
+        while True:
+            self.displayAccelerometerData()
+            self.displayMagnetometerData()
+            self.displayPressureData()
+            self.displayHumidityData()
+            
+            sleep(self.rateInSec)
+        
         
 test = HI2CSensorAdaptorTask()
